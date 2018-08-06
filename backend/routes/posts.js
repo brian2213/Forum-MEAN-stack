896ad0b70b4220aae038a7f2ab6adeae19afe9ca
brian <brian22e3@hotmail.com>
2018-08-06 14:22:02 -0700

    restructure backend functions as controller

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const Post = require('../models/post');
+
 const router = express.Router();
 const multer = require('multer');
 const checkAuth = require('../middleware/checkAuth');
+
+const postController = require('../controllers/post');
 
 const MIME_TYPE_MAP = {
   'image/png': 'png',
@@ -29,127 +31,24 @@ const storage = multer.diskStorage({
   }
 });
 
-router.delete('/:id', checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId })
-    .then(result => {
-      // console.log(result);
-      if (result.n > 0) {
-        res.status(200).json({ message: 'post deleted' });
-      } else {
-        res.status(401).json({ message: 'unauthorized deleted' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json({ message: 'fail to fetch posts' });
-    });
-});
+router.delete('/:id', checkAuth, postController.deletePost);
 
 router.post(
   '',
   checkAuth,
   multer({ storage: storage }).single('image'),
-  (req, res, next) => {
-    const url = req.protocol + '://' + req.get('host');
-    const post = new Post({
-      title: req.body.title,
-      content: req.body.content,
-      imagePath: url + '/images/' + req.file.filename,
-      creator: req.userData.userId
-    });
-
-    post
-      .save()
-      .then(result => {
-        res.status(201).json({
-          message: 'post send success',
-          post: {
-            id: result._id,
-            // title: result.title,
-            // content: result.content,
-            // imagePath: result.imagePath
-
-            ...result
-          }
-        });
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Creatring post failed' });
-      });
-  }
+  postController.createPost
 );
 
 router.put(
   '/:id',
   checkAuth,
   multer({ storage: storage }).single('image'),
-  (req, res, next) => {
-    let imagePath = req.body.imagePath;
-
-    if (req.file) {
-      const url = req.protocol + '://' + req.get('host');
-      imagePath = url + '/images/' + req.file.filename;
-    }
-
-    const post = new Post({
-      _id: req.body.id,
-      title: req.body.title,
-      content: req.body.content,
-      imagePath: imagePath,
-      creator: req.userData.userId
-    });
-    // console.log(post);
-    Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
-      .then(result => {
-        //console.log(result);
-        if (result.nModified > 0) {
-          res.status(200).json({ message: 'Update success' });
-        } else {
-          res.status(401).json({ message: 'Update failed' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'update failed' });
-      });
-  }
+  postController.updatePost
 );
 
-router.get('', (req, res, next) => {
-  const pageSize = +req.query.pageSize;
-  const curPage = +req.query.curPage;
-  const postQuery = Post.find();
-  let fetchedDoc;
-  if (pageSize && curPage) {
-    postQuery.skip(pageSize * (curPage - 1)).limit(pageSize);
-  }
-  postQuery
-    .then(document => {
-      fetchedDoc = document;
-      return Post.count();
-    })
-    .then(count => {
-      res.status(200).json({
-        message: 'posts send sucessfully',
-        posts: fetchedDoc,
-        maxPosts: count
-      });
-    })
-    .catch(error => {
-      res.status(500).json({ message: 'fail to fetch posts' });
-    });
-});
+router.get('', postController.getPosts);
 
-router.get('/:id', (req, res, next) => {
-  Post.findById(req.params.id)
-    .then(post => {
-      if (post) {
-        res.status(200).json(post);
-      } else {
-        res.status(404).json({ message: 'post not found' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json({ message: 'fail to fetch posts' });
-    });
-});
+router.get('/:id', postController.getPost);
 
 module.exports = router;
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -111,5 +111,4 @@ export class PostCreateComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.authStatusSub.unsubscribe();
   }
-
 }
